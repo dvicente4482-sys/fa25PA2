dec65e8d724c9f26894e1a73f5ae737e2d52c3fb
Dominic Vicente <dvicente4482@sdsu.edu>
2025-10-23 17:48:17 -0700

    Scrapped Original draft of push, realized I didnt actually have to manage parent and children in that method, got too confusing. Added seperate methods to work for parent/child

--- heap.h
+++ heap.h
@@ -10,54 +10,53 @@
 #include <vector>
 using namespace std;
 
-struct Node {
-    int weight = 0;
-    Node* left  = nullptr;
-    Node* right = nullptr;
-    // Optional: index if you’re mapping leaves to symbols
-    int index = -1;
-};
+
 
 struct MinHeap {
-    int data[64];
-    int size;
-    Node* root;      // head of the tree
-    Node* current;   // where you're attaching children
-    std::vector<Node> nodes;
+    int data[64]; //Array that stores the items into the heap, the items represent the index not their concrete values. Can connect this to WeightArr to achieve the actaul data.
+    int size; //Size of the heap
 
-    MinHeap() : size(0), root(nullptr), current(nullptr) {}
+
+    MinHeap() : size(0) {}
+        int parent(int i) {
+            return (i-1) / 2; //Represents Parent Node
+         }
+        int left(int i) {
+            return (2*i)+1; //Represents left node of the heap.
+        }
+        int right(int i) {
+            return (2*i)+2; //Represents the right node of the heap
+        }
 
     void push(int idx, int weightArr[]) {
         // TODO: insert index at end of heap, restore order using upheap()
-            if (size == 0) { //pushes to root if heap is empty
-                root = &nodes[idx];
-                current = root;
-                data[++size] = idx; //increments size and tracks the index
-                return;
-            } if (current->left == nullptr){ //USED to push to left node
-                current->left = &nodes[idx];
-                data[++size] = idx;
-                return;
-            }else if (current->right == nullptr) {//USED to push to right
-                current->right = &nodes[idx];
-                data[size++] = idx;
-                return;
-                //Unsure if works for heaps >3 logic looks right for <= 3
-            }
+        data[++size] = idx;
+        //upheap(node/pos, wieghtArr)
     }
 
-    int pop(int weightArr[]) { //Remove root, left right compared smaller value becomes root. perform upheap at root
+    int pop(int weightArr[]) {
+        //Remove root, left right compared smaller value becomes root. perform upheap at root
         // TODO: remove and return smallest index
         // Replace root with last element, then call downheap()
-        return -1; // placeholder
+        if (size == 0) {
+            return 0;
+        }
+        int minIdx = data[1];
+        size--;
+        data[1] = data[size];
+        //downheap(1,weightArr);
+
+        return minIdx; // placeholder
     }
 
-    void upheap(int pos, int weightArr[]) { //Resorts heap
+    void upheap(int pos, int weightArr[]) { //Resorts heap upwards
         // TODO: swap child upward while smaller than parent
+        //Implement the parent and Left/right methods to achieve the logic and function.
     }
 
-    void downheap(int pos, int weightArr[]) { //Resorts heap
+    void downheap(int pos, int weightArr[]) { //Resorts heap downwards
         // TODO: swap parent downward while larger than any child
+        //Implement the parent and Left/right methods to achieve the logic and function.
     }
 };
 
